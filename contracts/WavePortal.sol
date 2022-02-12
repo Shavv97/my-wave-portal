f877af812b3ff731d61ad1286863f703aefe697c
The Beast <johnnychavando5@gmail.com>
2022-02-11 16:30:07 -0800

    test deploy

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,9 +5,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
-  constructor () {
-    console.log("Welcome to the go to Social network of Web3");
+    uint256 totalWaves;
 
-     }
-  
-  }
+    constructor() {
+        console.log("Welcome to the premier Web3 social network");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+}
